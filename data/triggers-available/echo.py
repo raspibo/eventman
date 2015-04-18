75c83067d63708759f5f27fc65bd94a343f94b53
Davide Alberani <da@erlug.linux.it>
2015-04-18 15:02:20 +0200

    trigger example

---
+++ data/triggers-available/echo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""echo.py - Simply echo the environment and the stdin."""
+
+import os
+import sys
+import json
+
+def main():
+    print 'STDIN JSON:', json.loads(sys.stdin.read())
+    print ''
+    print 'ENV:', os.environ
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception, e:
+        print 'echo.py error: %s' % e
+        pass
