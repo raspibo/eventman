cf432cafd2a7fbd0c466cc08ff262bdce0560009
Davide Alberani <da@erlug.linux.it>
2017-04-28 23:10:01 +0200

    better debug strings

--- data/triggers/attends.d/print_label.py
+++ data/triggers/attends.d/print_label.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""print_label.py - print a label with the name, the company and the person_id (in a barcode) of an attendee
+"""print_label.py - print a label with the name, the company and the seq_hex key (in a barcode) of an attendee
 
 Copyright 2015-2017 Emiliano Mattioli <oloturia AT gmail.com>
                     Davide Alberani <da@erlug.linux.it>
@@ -58,34 +58,29 @@ def _get_resource(filename):
 
 
 def build_label(w, h, barcode_text, line1, line2, font_text=FONT_TEXT, font_barcode=FONT_BARCODE):
-    debug('build_label start')
     barcode_text = "*" + barcode_text + "*"
     fontbar = ImageFont.truetype(_get_resource(font_barcode), 1000)
     fontname = ImageFont.truetype(_get_resource(font_text), 520)
     fontjob = ImageFont.truetype(_get_resource(font_text), 360)
-    debug('build_label font done')
     image = Image.new('RGB', (w, h), (255, 255, 255))
     draw = ImageDraw.Draw(image)
     wbar, hbar = draw.textsize(barcode_text, font=fontbar)
     wnorm1, hnorm1 = draw.textsize(line1, font=fontname)
     wnorm2, hnorm2 = draw.textsize(line2, font=fontjob)
-    debug('build_label height calc done')
     draw.text(((w-wnorm1)/2, -670+(h-hnorm1)/2), line1, (0, 0, 0), font=fontname)
     draw.text(((w-wnorm2)/2, -220+(h-hnorm2)/2), line2, (0, 0, 0), font=fontjob)
     draw.text(((w-wbar)/2, 425+(h-hbar)/2), barcode_text, (0, 0, 0), font=fontbar)
-    debug('build_label draw text done')
     if not KEEP_IMG:
         tmpfile = tempfile.NamedTemporaryFile(prefix='eventman_print_label_', suffix='.png')
     else:
         tmpfile = tempfile.mktemp(prefix='eventman_print_label_', suffix='.png')
         tmpfile = open(tmpfile, 'wb')
     image.save(tmpfile, dpi=[600, 300], format='png')
-    debug('build_label end')
+    debug('print_label label image saved to %s' % tmpfile.name)
     return tmpfile
 
 
 def print_label(label_file, name):
-    debug('print_label start')
     printerName = PRINTER_NAME
     if os.environ.get('WEB_USER') in REMOTES:
         settings = REMOTES[os.environ['WEB_USER']]
@@ -104,11 +99,11 @@ def print_label(label_file, name):
     printer = printerName or conn.getDefault()
     debug('print_label connection done')
     conn.printFile(printer, label_file.name, name, {})
-    debug('print_label end')
+    debug('print_label print job sent')
 
 
 def run():
-    debug('run start')
+    debug('print_label start')
     # Always consume stdin.
     sys.stdin.read()
     name = os.environb.get(b'NAME') or b''
@@ -121,12 +116,12 @@ def run():
     seq = os.environ.get('SEQ_HEX', '0')
     label_file = build_label(LABEL_WIDTH, LABEL_HEIGHT, seq, fullname, company)
     print_label(label_file, fullname)
-    debug('run end')
+    debug('print_label completed')
 
 
 if __name__ == '__main__':
     try:
         run()
     except Exception as e:
-        sys.stderr.write('print_label.  Exception raised: %s\n' % str(e).encode('utf-8'))
+        sys.stderr.write('print_label.  Exception raised: %s\n' % str(e))
 
