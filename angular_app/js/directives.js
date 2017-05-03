c3f93609c11a28f27aafb8523ed36c9133bd62dd
Davide Alberani <da@erlug.linux.it>
2017-05-03 20:17:33 +0200

    fixes #181: press ESC to reset input field (list of tickets only)

--- angular_app/js/directives.js
+++ angular_app/js/directives.js
@@ -40,3 +40,16 @@ eventManApp.directive('resetFocus', function () {
     };
 });
 
+
+eventManApp.directive('escKey', function() {
+    return function(scope, element, attrs) {
+        element.bind('keydown keypress', function(evt) {
+            if (evt.which === 27) {
+                scope.$apply(function() {
+                    scope.$eval(attrs.escKey);
+                });
+                evt.preventDefault();
+            }
+        });
+    };
+});
